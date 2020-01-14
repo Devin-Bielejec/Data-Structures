a53b9e2d7b2c368624d96ec0bb21304c02fae22e
devin-bielejec <devin.bieleje@gmail.com>
2020-01-14 13:49:43 -0500

    Binary SEarch in order print

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -76,6 +76,10 @@ class BinarySearchTree:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
+        if node:
+            in_order_print(node.left)
+            print(node.value)
+            in_order_print(node.right)
         pass
 
     # Print the value of every node, starting with the given node,
